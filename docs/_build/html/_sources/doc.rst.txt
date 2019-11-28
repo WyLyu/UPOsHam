1d93fcdadd2ea86e8fe746a65a40273f64097191
naik_shibabrat <shiba@vt.edu>
2019-11-28 18:58:30 +0000

    set up autodocs

--- docs/_build/html/_sources/doc.rst.txt
+++ docs/_build/html/_sources/doc.rst.txt
@@ -7,8 +7,16 @@ Methods
 -------
 
 * Turning point
-* Turning point based on configuratio difference
+   .. automodule:: turning_point
+      :members:
+
+* Turning point based on configuration difference
+   .. automodule:: turning_point_coord_difference
+      :members:
+
 * Differential correction
+   .. automodule:: differential_correction
+      :members:
 
 
 Problems
--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.intersphinx','recommonmark']
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 source_suffix = ['.rst']
 
